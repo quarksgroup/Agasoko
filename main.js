5450cd22f355a254ca347b771da7446be176d33d
hakizimana bahati <hakizimanabahati2019@gmail.com>
2021-10-28 05:34:50 -0700

    eleventh commit

--- main.js
+++ main.js
@@ -1,35 +1,40 @@
-function selector(element){
-    return document.querySelector(element)
+window.onload = function () {
+    function selector(element) {
+        return document.querySelector(element)
+    }
+
+    let form = selector('#productform');
+
+    form.addEventListener("submit", function (e) {
+
+        e.preventDefault();
+        console.log("fileReader.result")
+
+
+        let name = selector("#productName").value;
+        let color = selector("#productColor").value;
+        let price = selector("#productPrice").value;
+
+
+        selector(".product-name").innerHTML = name;
+        selector(".product-price").innerHTML = price;
+        selector(".productBox").style.backgroundColor = color;
+
+        let file = selector("#file").files[0];
+        console.log(file);
+
+        let fileReader = new FileReader();
+        if (file) {
+            fileReader.readAsDataURL(file);
+        }
+
+        fileReader.onloadend = function () {
+            console.log(fileReader.result);
+            selector("#image").src = fileReader.result;
+        }
+
+
+        selector(".productBox").classList.remove("hidden");
+
+    })
 }
-
-let form = selector('#productForm');
-
-
-form.addEventListener("submit", function(e) {
-
-    e.preventDefault();
-    
-
-    let name =  selector("#productName").value;
-    let color =  selector("#productColor").value;
-    let price =  selector("#productPrice").value;
-
-
-    selector(".product-name").innerHTML=name;
-    selector(".product-price").innerHTML=price;
-    selector(".productBox").style.backgroundColor=color;
-
-    let file = selector("#file").files[0];
-
-    let fileReader = new FileReader();
-
-    fileReader.onloadend = function(){
-        selector("#image").src = fileReader.result;
-    }
-
-    if(file){
-        fileReader.readAsDataURL(file);
-    }
-    selector(".productBox").classList.remove("hidden");
-
-})
